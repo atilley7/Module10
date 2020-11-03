b0ecc0bc0f6a0b88e226de104f8bf36378c3833e
atilley7 <averytilley7@gmail.com>
2020-11-02 22:30:55 -0600

    2nd test

--- test/student_test.py
+++ test/student_test.py
@@ -4,7 +4,7 @@ from class_definitions import student as s
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = s.Student("Tilley", "Avery", 'Buisness', 4.0)
+        self.student = s.Student("Tilley", "Avery", 'Business', 4.0)
 
     def tearDown(self):
         del self.student
@@ -12,7 +12,14 @@ class MyTestCase(unittest.TestCase):
     def test_object_created_required_attributes(self):
         self.assertEqual(self.student.last_name, 'Tilley')
         self.assertEqual(self.student.first_name, 'Avery')
-        self.assertEqual(self.student.major, 'Buisness')
+        self.assertEqual(self.student.major, 'Business')
+
+    def test_object_created_all_attributes(self):
+        student = s.Student('Tilley', 'Avery', 'Business', 4.0)
+        assert student.last_name == 'Tilley'
+        assert student.first_name == 'Avery'
+        assert student.major == 'Business'
+        assert student.gpa == 4.0
 
 
 if __name__ == '__main__':
